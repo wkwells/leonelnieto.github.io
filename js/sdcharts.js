e29869cffdf101912cd58dbe35fba9aa751736af
Leonel Nieto <43765662+leonelnieto@users.noreply.github.com>
2018-12-10 07:00:50 -0700

    Deuteronomy
    Weekend upates: addded strategic charts.. added tactical measures links.. added charts to funding section..

--- js/sdcharts.js
+++ js/sdcharts.js
@@ -1,110 +1,214 @@
-var revcanvas = document.getElementById("TIFRevenuePieChart");
-var revctx = revcanvas.getContext('2d');
-var theHelp = Chart.helpers;
 
-var expcanvas = document.getElementById("TIFExpendituresPieChart");
-var expctx = expcanvas.getContext('2d');
-
-var tifrevenue = {
-  labels: ["Motor Fuel / Special Fuel", "Motor Vehicle Registration","Sales Tax"],
-  datasets: [{
-    fill: true,
-    backgroundColor:["#6d3610","#ee7623","#f5b487"],
-    data:[5,12,83],
-    borderColor: ['#fff', '#fff',"#fff"],
-    borderWidth: [3, 3]
-  }]
-};
-var tifexpenditures = {
-  labels: ["Bond Payments", "Transit Transportation Investment Fund","Current Projects"],
-  datasets: [{
-    fill: true,
-    backgroundColor:["#6d3610","#ee7623","#f5b487"],
-    data:[43,0,57],
-    borderColor: ['#fff', '#fff',"#fff"],
-    borderWidth: [3, 3]
-  }]
-};
-var options = {
-  rotation: -0.7 * Math.PI,
-  legend: {
-    display: true,
-    position: 'bottom',
-    animation: {
-      animateScale: true,
-      animateRotate: true
-    },
-    // generateLabels changes from chart to chart,  check the source,
-    // this one is from the doughut :
-    // https://github.com/chartjs/Chart.js/blob/master/src/controllers/controller.doughnut.js#L42
-    labels: {
-      generateLabels: function(chart) {
-        var data = chart.data;
-        if (data.labels.length && data.datasets.length) {
-          return data.labels.map(function(label, i) {
-            var meta = chart.getDatasetMeta(0);
-            var ds = data.datasets[0];
-            var arc = meta.data[i];
-            var custom = arc && arc.custom || {};
-            var getValueAtIndexOrDefault = theHelp.getValueAtIndexOrDefault;
-            var arcOpts = chart.options.elements.arc;
-            var fill = custom.backgroundColor ? custom.backgroundColor : getValueAtIndexOrDefault(ds.backgroundColor, i, arcOpts.backgroundColor);
-            var stroke = custom.borderColor ? custom.borderColor : getValueAtIndexOrDefault(ds.borderColor, i, arcOpts.borderColor);
-            var bw = custom.borderWidth ? custom.borderWidth : getValueAtIndexOrDefault(ds.borderWidth, i, arcOpts.borderWidth);
-              return {
-              // And finally :
-              text: ds.data[i] + "% " + label,
-              fillStyle: fill,
-              strokeStyle: stroke,
-              lineWidth: bw,
-              hidden: isNaN(ds.data[i]) || meta.data[i].hidden,
-              index: i
-            };
-          });
-        }
-        return [];
-      }
-    }
-  }
-};
-// Chart declaration:
-var myPieChart = new Chart(revctx, {
-  type: 'doughnut',
-  data: tifrevenue,
-  options: options
-});
-var expPieChart = new Chart(expctx, {
-  type: 'doughnut',
-  data: tifexpenditures,
-  options: options
-});
-//console.log(myPieChart.generateLegend());
-//Plugin from githubExample:
-//https://github.com/chartjs/Chart.js/blob/master/samples/data_labelling.html
-Chart.plugins.register({
-  afterDatasetsDraw: function(chartInstance, easing) {
-    // To only draw at the end of animation, check for easing === 1
-    var ctx = chartInstance.chart.ctx;
-    chartInstance.data.datasets.forEach(function(dataset, i) {
-      var meta = chartInstance.getDatasetMeta(i);
-      if (!meta.hidden) {
-        meta.data.forEach(function(element, index) {
-          // Draw the text in black, with the specified font
-          ctx.fillStyle = 'black';
-          var fontSize = 12;
-          var fontStyle = 'normal';
-          var fontFamily = 'Muli';
-          ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
-          // Just naively convert to string for now
-          var dataString = dataset.data[index].toString();
-          // Make sure alignment settings are correct
+//Strategic Goal Charts
+function drawGoalCharts() {
+  var url = "https://dashboard.udot.utah.gov/resource/rqv9-ry2j.json?entity=Statewide";
+  fetch(url).then(function(response){
+    return response.json();
+  }).then(function(data){
+    var dataIndex = data[0].safety;
+    var indexLabel =["","Safety Index"];
+    Chart.pluginService.register({
+  		beforeDraw: function (chart) {
+  			if (chart.config.options.elements.center) {
+          //Get ctx from string
+          var ctx = chart.chart.ctx;
+  				//Get options from the center object in options
+          var centerConfig = chart.config.options.elements.center;
+        	var fontStyle = centerConfig.fontStyle || 'Arial';
+  				var txt = centerConfig.text;
+          var color = centerConfig.color || '#000';
+          var sidePadding = centerConfig.sidePadding || 20;
+          var sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
+          //Start with a base font of 30px
+          ctx.font = "30px " + fontStyle;
+  				//Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+          var stringWidth = ctx.measureText(txt).width;
+          var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+          // Find out how much the font can grow in width.
+          var widthRatio = elementWidth / stringWidth;
+          var newFontSize = Math.floor(30 * widthRatio);
+          var elementHeight = (chart.innerRadius * 2);
+          // Pick a new font size so it will not be larger than the height of label.
+          var fontSizeToUse = Math.min(newFontSize, elementHeight);
+  				//Set font settings to draw it correctly.
           ctx.textAlign = 'center';
           ctx.textBaseline = 'middle';
-          var padding = 5;
-          var position = element.tooltipPosition();
-          ctx.fillText(dataString + '%', position.x, position.y - (fontSize / 2) - padding);
-        });
-      }
+          var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+          var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+          ctx.font = fontSizeToUse+"px " + fontStyle;
+          ctx.fillStyle = color;
+          //Draw text in center
+          ctx.fillText(txt, centerX, centerY);
+  			}
+  		}
     });
-  }
-});
+		var config = {
+			type: 'doughnut',
+			data: {
+				labels: indexLabel,
+				datasets: [{
+					data: [(Math.round((100-dataIndex)*10)/10),dataIndex],
+					backgroundColor: [
+					  "#5a87c5",
+					  "#d58e61"
+					]
+				}]
+			},
+		options: {
+      defaultFontFamily: Chart.defaults.global.defaultFontFamily = "proxima-nova, sans-serif",
+      legend:{display:false},
+    	responsive: true,
+      animation: {duration: 3000, animateScale: true,animateRotate: true,easing:'easeOutBack'},
+			elements: {
+				center: {
+					text: dataIndex+'%',
+          color: '#000000', // Default is #000000
+          fontStyle: 'proxima-nova, sans-serif', // Default is Arial
+          sidePadding: 20 // Defualt is 20 (as a percentage)
+				}
+			}
+		}
+	 };
+  	var ctx = document.getElementById("zero-fatalities-doughut-chart").getContext("2d");
+  	var myChart = new Chart(ctx, config);
+    dataIndex = data[0].mobility;
+    indexLabel =["","Mobility Index"];
+    config = {
+			type: 'doughnut',
+			data: {
+				labels: indexLabel,
+				datasets: [{
+					data: [(Math.round((100-dataIndex)*10)/10),dataIndex],
+					backgroundColor: [
+					  "#5a87c5",
+					  "#d58e61"
+					]
+				}]
+			},
+		options: {
+      defaultFontFamily: Chart.defaults.global.defaultFontFamily = "proxima-nova, sans-serif",
+      legend:{display:false},
+    	responsive: true,
+      animation: {duration: 3000, animateScale: true,animateRotate: true,easing:'easeOutBack'},
+			elements: {
+				center: {
+					text: dataIndex+'%',
+          color: '#000000', // Default is #000000
+          fontStyle: 'proxima-nova, sans-serif', // Default is Arial
+          sidePadding: 20 // Defualt is 20 (as a percentage)
+				}
+			}
+		}
+	 };
+    var ctx = document.getElementById("optimize-mobility-doughut-chart").getContext("2d");
+  	var myChart = new Chart(ctx, config);
+    dataIndex = data[0].infrastructure;
+    indexLabel =["","Infrastructure Index"];
+    config = {
+			type: 'doughnut',
+			data: {
+				labels: indexLabel,
+				datasets: [{
+					data: [(Math.round((100-dataIndex)*10)/10),dataIndex],
+					backgroundColor: [
+					  "#5a87c5",
+					  "#d58e61"
+					]
+				}]
+			},
+		options: {
+      defaultFontFamily: Chart.defaults.global.defaultFontFamily = "proxima-nova, sans-serif",
+      legend:{display:false},
+    	responsive: true,
+      animation: {duration: 3000, animateScale: true,animateRotate: true,easing:'easeOutBack'},
+			elements: {
+				center: {
+					text: dataIndex+'%',
+          color: '#000000', // Default is #000000
+          fontStyle: 'proxima-nova, sans-serif', // Default is Arial
+          sidePadding: 20 // Defualt is 20 (as a percentage)
+				}
+			}
+		}
+	 };
+    var ctx = document.getElementById("preserve-infrastructure-doughut-chart").getContext("2d");
+  	var myChart = new Chart(ctx, config);
+    //Fron here on down draw historical charts.
+    url = "https://dashboard.udot.utah.gov/resource/b8iq-pg44.json?$select=year,avg(safety),avg(mobility),avg(infrastructure)&$group=year&$order=year";
+    fetch(url).then(function(response){
+      return response.json();
+    }).then(function(j){
+      var zfData = [];
+      var omData = [];
+      var piData = [];
+      var years = [];
+      for(var i = 0; i < j.length; i++){
+          zfData.push(parseFloat(j[i]["avg_safety"]).toFixed(2));
+          omData.push(parseFloat(j[i]["avg_mobility"]).toFixed(2));
+          piData.push(parseFloat(j[i]["avg_infrastructure"]).toFixed(2));
+          years.push(j[i]["year"]);
+      }
+      var zfLineChart = document.getElementById("zero-fatalities-line-chart");
+      var omLineChart = document.getElementById("optimize-mobility-line-chart");
+      var piLineChart = document.getElementById("preserve-infrastructure-line-chart");
+      Chart.defaults.global.defaultFontFamily = "proxima-nova, sans-serif";
+      Chart.defaults.global.defaultFontSize = 18;
+      var linechartData = {
+        labels: years,
+        datasets: [{
+          label: "Safety Index",
+          data: zfData,
+          borderColor: "#5a87c5"
+        }]
+      };
+      var chartOptions = {
+        responsive: true,
+        animation: {duration: 3000, animateScale: true,animateRotate: true,easing:'easeOutBack'},
+        legend: {
+          display: true,
+          position: 'top',
+          labels: {
+            boxWidth: 80
+          }
+        }
+      };
+      new Chart(zfLineChart, {
+        type: 'line',
+        data: linechartData,
+        options: chartOptions
+      });
+      //redefine data
+      linechartData = {
+        labels: years,
+        datasets: [{
+          label: "Mobility Index",
+          data: omData,
+          borderColor: "#5a87c5"
+        }]
+      };
+      new Chart(omLineChart, {
+        type: 'line',
+        data: linechartData,
+        options: chartOptions
+      });
+      //redefine data
+      linechartData = {
+        labels: years,
+        datasets: [{
+          label: "Infrastructure Index",
+          data: piData,
+          borderColor: "#5a87c5"
+        }]
+      };
+      new Chart(piLineChart, {
+        type: 'line',
+        data: linechartData,
+        options: chartOptions
+      });
+    }).catch(function(err){
+      console.log("(*_*) if you see me there is with the second fetch..."+err);
+    });
+  }).catch(function(err){
+    console.log("{*_*} if you see me there is problem..."+err);
+  });
+}
